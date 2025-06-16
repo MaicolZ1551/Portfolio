90bbce9611d105d8ca66be504422c98542b3902b
Maicol Z <maicolzuluagaarias591@gmail.com>
2025-06-16 18:19:26 -0500

    The button for the Calculator project now correctly redirects to the Netlify site.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//link GitHub
 function GitHubLink(url) {
   const elements = document.querySelectorAll(".info2");
   elements.forEach(e => {
@@ -11,7 +12,7 @@ GitHubLink("https://github.com/MaicolZ1551");
 
 
 
-
+//link linkedIn
 function LinkedInLink(url) {
   const elements = document.querySelectorAll(".info4");
   elements.forEach(e => {
@@ -25,7 +26,7 @@ LinkedInLink("https://www.linkedin.com/in/maicol-zuluaga-arias-b1661836a/");
 
 
 
-
+//link digital clock
 function project1(url) {
   const elements = document.querySelectorAll(".project1");
   elements.forEach(e => {
@@ -36,3 +37,17 @@ function project1(url) {
 }
 // Call Link
 project1("https://digital-clock741.netlify.app");
+
+
+
+//link calculator
+function project2(url) {
+  const elements = document.querySelectorAll(".project2");
+  elements.forEach(e => {
+    e.addEventListener("click", () => {
+      window.open(url, "_blank", "noopener");
+    });
+  });
+}
+// Call Link
+project2("https://redcalculator741.netlify.app");
